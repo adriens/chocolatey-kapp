4f9d710c155740a1247886ca95dd67c893215425
adriens <Adrien.Sales@gmail.com>
2020-12-12 09:50:44 +1100

    To implement moderation requirements.

---
+++ tools/chocolateyUninstall.ps1
@@ -0,0 +1,7 @@
+﻿$tools = Split-Path $MyInvocation.MyCommand.Definition
+$content = Join-Path (Split-Path $tools) 'content'
+$bat = Join-Path $content 'kapp'
+
+Uninstall-BinFile `
+  -Name 'kapp' `
+  -Path $bat
